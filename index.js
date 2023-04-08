9431fe132eeb0d30a2f3b5b74712bb4a8264c6d7
Samskido <onyekweresamskido@gmail.com>
2023-04-08 19:18:59 +0100

    sidehustle week 3 task

---
+++ index.js
@@ -0,0 +1,21 @@
+function toggleMobileMenu(menu) {
+  menu.classList.toggle("open");
+ }
+
+$(document).ready(function() {
+    $('.popup-button').click(function(e) {
+      $('.wrapper').fadeIn(500);
+      $('.popup-box').removeClass('transform-out').addClass('transform-in');
+  
+      e.preventDefault();
+    });
+  
+    $('.popup-close').click(function(e) {
+      $('.wrapper').fadeOut(500);
+      $('.popup-box').removeClass('transform-in').addClass('transform-out');
+  $(`.succesful`).removeClass(`transform-in`).addClass("transform-out")
+      e.preventDefault();
+     
+    });
+  });
+ 
